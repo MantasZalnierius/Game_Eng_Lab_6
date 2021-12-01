636373cc25b58f42bf8ce0c6fd11d71d7174f87c
Mantas Zalnierius <C00241626@itcarlow.ie>
2021-12-01 14:58:10 +0000

    Finished lab 6

--- MainLine/Include/LotteryProduction.h
+++ MainLine/Include/LotteryProduction.h
@@ -5,7 +5,9 @@ class LotteryProduction
 {
 public:
     LotteryProduction();
+    void run();
 	std::pair<bool, std::set<int>> validateLottery(std::set<int> lotteryNumber);
 private:
     int m_numberOfRandomNumbers;
+    const int MAX_INPUTS{6};
 };
--- MainLine/Src/Mainline.cpp
+++ MainLine/Src/Mainline.cpp
@@ -6,43 +6,8 @@
 int main()
 {
     srand(static_cast<unsigned>(time(nullptr)));
-	int NumberOfInputs = 6;
-	std::set<int> lotteryNumbers;
 	LotteryProduction lottery;
-	bool retryLottery = true;
-	while(retryLottery)
-	{
-		std::cout << "ENTER 6 NUMBERS, ONE BY ONE, FROM RANGE 1 TO 46" << std::endl;
-		while(NumberOfInputs > 0)
-		{
-			int input;
-			std::cin >> input;
-			lotteryNumbers.insert(input);
-			NumberOfInputs--;
-		}
-		if(lottery.validateLottery(lotteryNumbers).first)
-		{
-			std::cout << "THIS IS A VALID LOTTERY" << std::endl;
-		}
-		else
-		{
-			std::cout << "THIS IS NOT A VALID LOTTERY" << std::endl;
-		}
-		std::cout << "DO YOU WANT TO ENTER ANOTHER LOTTERY (1: YES, 2 NO)" << std::endl;
-		int answer;
-		std::cin >> answer;
-		if(answer == 1)
-		{
-			retryLottery = true;
-			NumberOfInputs = 6;
-			lotteryNumbers.clear();
-			system("CLS");
-		}
-		else
-		{
-			retryLottery = false;
-		}
-	}
+	lottery.run();
 	std::cin.get();
 	return 0;
 }
